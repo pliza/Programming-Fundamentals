805d5259f669abee9260e1be6ff9aa5950b3adef
pliza <rabirokhliza@gmail.com>
2024-09-25 16:23:56 +0300

    1.5

--- labs/1.5/Lab_1.5.cpp
+++ labs/1.5/Lab_1.5.cpp
@@ -1,6 +1,48 @@
+/**
+* Done by:
+ * Student Name: Liza Rabirokh
+ * Student Group: 121
+ 
+ */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
 int main()
 {
-    int a = 6;
+    {
+    float fltA = 45;
+    int nB1 = 13;
+    int nC = 47;
+    int nD1 = 59;
+    bool bRez1 = !( ! (fltA > nB1) && ! (nC > nD1));
 
-    std::cout << a;
+    double nA = 84.6;
+    double nB2 = 84.6;
+    int nD2 = 63;
+    float fltC = 34;
+    bool bRez2 = !( ! (nA > nB2) && !(fltC > nD2));
+    }
+//A <БО1> <УО> B <АО1> <СО> C <ОВ> D <АО2> E <БО2> <БазО> F
+//БО1БО2АО1АО2 УО СО БазО ОВ
+//& >> + *    ~ * sizeof !=
+{
+    int const CONSTA = 71; 
+    int nB, nD, nE;
+    nB = 15;
+    nD = 1;
+    nE = 49;
+    float ptr= 53.4;
+    float* fltG = &ptr;
+    short sh = 34;
+    bool bRez = (((CONSTA &(~nB))+ (*fltG)) != (nD * (nE >> sizeof(34))));
+    cout << bRez;
+
 }
+return 0;
+}
+
+
+
+
