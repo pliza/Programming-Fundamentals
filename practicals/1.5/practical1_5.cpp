93c591ba029a55fbf3c8505e74d5f30e3cc11504
pliza <rabirokhliza@gmail.com>
2024-10-02 21:50:28 +0300

    1.5

--- practicals/1.5/practical1_5.cpp
+++ practicals/1.5/practical1_5.cpp
@@ -9,22 +9,67 @@
 #include <vector>
 using namespace std;
 
-int main()
-{
-    vector <int> arr = {2, 5, 8, 45, 87, 23, 87};
-    int P = 87;
-    for (int i = 0; i<arr.size(); i++)
-    {if (arr[i] == P )
+int main(){
+   {
+      vector <int> arr = {2, 5, 8, 45, 87, 23, 87};
+      int P = 87;
+      for (int i = 0; i<arr.size(); i++){
+         if (arr[i] == P ){
+            cout<<i<<endl;
+            break;
+         }
+      }
+   }
+//Задано послідовність значень А[n]. Знайти найменше значення серед
+// додатних елементів послідовності А[n].
      {
-        cout<<i<<endl;
-        break;
-     }
+        vector <int> arr = {5, -4, 67, 3, 2, -72, 1 };
+        int min = arr[0];
+        for (int i = 1; i<arr.size(); i++)
+        {
+         if (arr[i]>0) {
+         if (arr[i]< min){
+            min = arr[i];
+         }
+        }
 
-     {
-        vector <int> arr = {5, 9, -67, -43, 73, -72 };
-        
+        }
+         cout<<min<<endl;   
      }
+      
+      
+   //3. Задано послідовність значень А[n]. Знайти найбільше і найменше значення та поміняти їх місцями
+   {
+   vector <int> arr = {5, -4, -67, 3, -72, 1 };
+   int min = arr[0];
+   int max = arr[0];
+   int arrmin = 0;
+   int arrmax = 0;
+        for (int i = 1; i<arr.size(); i++)
+      {
+         if (arr[i]< min){
+            min = arr[i];
+            arrmin = i;
+         }
+          if (arr[i]> max){
+            max = arr[i];
+            arrmax = i;
+      
+
+            
+        }
+      }
+      arr[arrmin]= max;
+      arr[arrmax] = min;
+      
+      cout << max << endl;
+      cout << min << endl;
+   }
+}
+
+
+
+   
     
-    }
 
-}    
+ 
