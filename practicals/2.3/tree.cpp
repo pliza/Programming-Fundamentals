0da48c86fb06975031979b6d427d223aff68f3da
pliza <rabirokhliza@gmail.com>
2024-10-14 22:17:58 +0300

    tree

--- practicals/2.3/tree.cpp
+++ practicals/2.3/tree.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 int main()
 {
+    ofstream outFile("cristmas_tree.txt");
+
     string green = "\033[32m";
     string brown = "\033[33m";
     string reset = "\033[0m"; // Скидання кольору
@@ -24,44 +26,51 @@ int main()
 
     for (int level = 1; level <= n; level++)
     {
-        int rows = level + 1;
+        int rows = level + 2;
         for (int row = 1; row <= rows; row++)
         {
-            // Друкуємо пробіли для вирівнювання трикутника
-            for (int space = 1; space <= n + rows - row; space++)
+            for (int space = 2; space <= n + 3 - row; space++)
             {
                 cout << " ";
+                outFile << " ";
             }
             cout << green;
             for (int star = 1; star <= 2 * row - 1; star++)
             {
                 if (rand() % 5 == 0)
                 {
-                    cout << toys[rand() % toys.length()];
+                    char toy = toys[rand() % toys.length()];
+                    cout << toy;
+                    outFile << toy;
                 }
                 else
                 {
                     cout << "*";
+                    outFile << "*";
                 }
             }
+
             cout << reset << endl;
+            outFile << endl;
         }
     }
-
     int trunkHeight = 2;
     int trunkWidth = 3;
     for (int t = 0; t < trunkHeight; ++t)
     {
-        for (int space = 1; space <= n + 1; space++)
+        for (int space = 1; space <= n; space++)
         {
             cout << " ";
+            outFile << " ";
         }
         cout << brown;
         for (int star = 1; star <= trunkWidth; star++)
         {
             cout << "*";
+            outFile << "*";
         }
         cout << reset << endl;
+        outFile << endl;
     }
 
     return 0;
